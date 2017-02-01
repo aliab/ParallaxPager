1186c052fd2a4026c50c1ed31ccf45cd25f57298
Ali Abdolahi <i@abdolahi.info>
2017-02-01 20:31:28 +0330

    make it custom hamsaa one

--- parallaxpager/src/main/java/com/prolificinteractive/parallaxpager/ParallaxFactory.java
+++ parallaxpager/src/main/java/com/prolificinteractive/parallaxpager/ParallaxFactory.java
@@ -3,63 +3,82 @@ package com.prolificinteractive.parallaxpager;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class ParallaxFactory {
 
-  private final List<OnViewCreatedListener> otherListeners;
+    private final List<OnViewCreatedListener> otherListeners;
 
-  public ParallaxFactory(OnViewCreatedListener... others) {
-    otherListeners = Arrays.asList(others);
-  }
-
-  /**
-   * Handle the created view
-   *
-   * @param view nullable.
-   * @param context shouldn't be null.
-   * @param attrs shouldn't be null.
-   * @return null if null is passed in.
-   */
-
-  public View onViewCreated(View view, Context context, AttributeSet attrs) {
-    if (view == null) {
-      return null;
+    public ParallaxFactory(OnViewCreatedListener... others) {
+        otherListeners = Arrays.asList(others);
     }
 
-    view = onViewCreatedInternal(view, context, attrs);
-    for (OnViewCreatedListener listener : otherListeners) {
-      if (listener != null) {
-        view = listener.onViewCreated(view, context, attrs);
-      }
-    }
-    return view;
-  }
+    /**
+     * Handle the created view
+     *
+     * @param view    nullable.
+     * @param context shouldn't be null.
+     * @param attrs   shouldn't be null.
+     * @return null if null is passed in.
+     */
 
-  private View onViewCreatedInternal(View view, final Context context, AttributeSet attrs) {
+    public View onViewCreated(View view, Context context, AttributeSet attrs) {
+        if (view == null) {
+            return null;
+        }
 
-    int[] attrIds =
-        { R.attr.a_in, R.attr.a_out, R.attr.x_in, R.attr.x_out, R.attr.y_in, R.attr.y_out, R.attr.override_visibility };
-
-    TypedArray a = context.obtainStyledAttributes(attrs, attrIds);
-
-    if (a != null) {
-      if (a.length() > 0) {
-        ParallaxViewTag tag = new ParallaxViewTag();
-        tag.alphaIn = a.getFloat(0, 0f);
-        tag.alphaOut = a.getFloat(1, 0f);
-        tag.xIn = a.getFloat(2, 0f);
-        tag.xOut = a.getFloat(3, 0f);
-        tag.yIn = a.getFloat(4, 0f);
-        tag.yOut = a.getFloat(5, 0f);
-        tag.overrideVisibility = a.getBoolean(6, false);
-        view.setTag(R.id.parallax_view_tag, tag);
-      }
-      a.recycle();
+        view = onViewCreatedInternal(view, context, attrs);
+        for (OnViewCreatedListener listener : otherListeners) {
+            if (listener != null) {
+                view = listener.onViewCreated(view, context, attrs);
+            }
+        }
+        return view;
     }
 
-    return view;
-  }
+    private View onViewCreatedInternal(View view, final Context context, AttributeSet attrs) {
+
+        int[] attrIds =
+                {R.attr.a_in_left, R.attr.a_in_right, R.attr.a_out_left, R.attr.a_out_right,
+                        R.attr.x_in_left, R.attr.x_in_right, R.attr.x_out_left, R.attr.x_out_right,
+                        R.attr.y_in_left, R.attr.y_in_right, R.attr.y_out_left, R.attr.y_out_right,
+                        R.attr.override_visibility,
+                        R.attr.p_name};
+
+        TypedArray a = context.obtainStyledAttributes(attrs, attrIds);
+
+        if (a != null) {
+            if (a.length() > 0) {
+                ParallaxViewTag tag = new ParallaxViewTag();
+                tag.alphaInLeft = a.getFloat(0, 0f);
+                tag.alphaInRight = a.getFloat(1, 0f);
+                tag.alphaOutLeft = a.getFloat(2, 0f);
+                tag.alphaOutRight = a.getFloat(3, 0f);
+                tag.xInLeft = a.getFloat(4, 0f);
+                tag.xInRight = a.getFloat(5, 0f);
+                tag.xOutLeft = a.getFloat(6, 0f);
+                tag.xOutRight = a.getFloat(7, 0f);
+                tag.yInLeft = a.getFloat(8, 0f);
+                tag.yInRight = a.getFloat(9, 0f);
+                tag.yOutLeft = a.getFloat(10, 0f);
+                tag.yOutRight = a.getFloat(11, 0f);
+                tag.overrideVisibility = a.getBoolean(12, false);
+                float f = a.getFloat(13,0);
+                String name = "";
+                if (f != 1)
+                    name = "empty";
+                else
+                    Log.e("view.Name",name);
+                tag.name = name;
+                view.setTag(R.id.parallax_view_tag, tag);
+            }
+            a.recycle();
+        }
+
+        return view;
+    }
 }
--- parallaxpager/src/main/java/com/prolificinteractive/parallaxpager/ParallaxViewTag.java
+++ parallaxpager/src/main/java/com/prolificinteractive/parallaxpager/ParallaxViewTag.java
@@ -2,11 +2,18 @@ package com.prolificinteractive.parallaxpager;
 
 public class ParallaxViewTag {
   protected int index;
-  protected float xIn;
-  protected float xOut;
-  protected float yIn;
-  protected float yOut;
-  protected float alphaIn;
-  protected float alphaOut;
+  protected float xInLeft;
+  protected float xInRight;
+  protected float xOutLeft;
+  protected float xOutRight;
+  protected float yInLeft;
+  protected float yInRight;
+  protected float yOutLeft;
+  protected float yOutRight;
+  protected float alphaInLeft;
+  protected float alphaInRight;
+  protected float alphaOutLeft;
+  protected float alphaOutRight;
   protected boolean overrideVisibility;
+  protected String name;
 }
